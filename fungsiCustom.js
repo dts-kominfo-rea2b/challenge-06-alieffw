df0e53f6ff247a6b9ccfb185feb1c92733b2ff64
aliefwijaya <alieffitriyanto92@gmail.com>
2022-06-11 16:07:09 +0700

    challenge-06-alieffw

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fileStream = require('fs');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,47 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+let file = [file1, file2, file3];
+
+const bacaData = (fnCallBack) => {
+  let save = [];
+  let kata = null;
+  fileStream.readFile(file1, {encoding:'utf-8'}, (err, data)=> {
+    if(err) return err;
+    kata = newWord(data);
+    save.push(kata);
+    fileStream.readFile(file2,{encoding: 'utf-8'},(err, data) => {
+      if(err) return err;                              
+      kata = newWord(data);
+      save.push(kata);
+      fileStream.readFile(file3,{encoding: 'utf-8'},(err, data) => {
+                      if (err) return err;             
+                      kata = newWord(data);
+                      save.push(kata);                          
+                      //console.log(save +"\n");
+                      fnCallBack(null, save);
+  })
+})
+})
+}
+
+const newWord = (data) =>{
+  const arrayObject = JSON.parse(data);
+  let splitData = null;
+  if(arrayObject.message !== undefined){
+    splitData = arrayObject.message.split(" ");
+    return splitData[1];
+   //return index;
+  }else  if(arrayObject[0].message !== undefined){
+    splitData = arrayObject[0].message.split(" ");
+    return splitData[1];
+   //return index;
+  }else{
+    splitData = arrayObject[0].data.message.split(" ");
+    return splitData[1];
+    //return index;
+  }
+}
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
